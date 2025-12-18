0edabc99ba210de915936f1131118d95dddfc3ae
Egor Egorov <me@egorfine.com>
2025-12-18 14:48:44 +0100

    initial

---
+++ Formula/libzip@1.11.4.rb
@@ -0,0 +1,56 @@
+class LibzipAT1114 < Formula
+  desc "C library for reading, creating, and modifying zip archives"
+  homepage "https://libzip.org/"
+  url "https://libzip.org/download/libzip-1.11.4.tar.xz"
+  sha256 "8a247f57d1e3e6f6d11413b12a6f28a9d388de110adc0ec608d893180ed7097b"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url "https://libzip.org/download/"
+    regex(/href=.*?libzip[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  no_autobump! because: "unable to get versions"
+
+  depends_on "cmake" => :build
+  depends_on "xz"
+  depends_on "zstd"
+
+  uses_from_macos "zip" => :test
+  uses_from_macos "bzip2"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "openssl@3"
+  end
+
+  conflicts_with "libtcod", because: "libtcod and libzip install a `zip.h` header"
+
+  def install
+    ENV['MACOSX_DEPLOYMENT_TARGET']="13.0"
+    args = %w[
+      -DENABLE_GNUTLS=OFF
+      -DENABLE_MBEDTLS=OFF
+      -DBUILD_REGRESS=OFF
+      -DBUILD_EXAMPLES=OFF
+      -DBUILD_SHARED_LIBS=OFF
+      -DBUILD_TOOLS=OFF
+      -DBUILD_REGRESS=OFF
+      -DBUILD_OSSFUZZ=OFF
+      -DBUILD_DOC=OFF
+    ]
+    args << "-DENABLE_OPENSSL=OFF" if OS.mac? # Use CommonCrypto instead.
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    touch "file1"
+    system "zip", "file1.zip", "file1"
+    touch "file2"
+    system "zip", "file2.zip", "file1", "file2"
+    assert_match(/\+.*file2/, shell_output("#{bin}/zipcmp -v file1.zip file2.zip", 1))
+  end
+end

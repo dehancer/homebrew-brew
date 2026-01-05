b4b3107b688656bcf753304d3d3688bd0a798aad
Egor Egorov <me@egorfine.com>
2026-01-05 16:17:16 +0100

    libomp

---
+++ Formula/libomp_dehancer.rb
@@ -0,0 +1,86 @@
+class LibompDehancer < Formula
+  desc "LLVM's OpenMP runtime library"
+  homepage "https://openmp.llvm.org/"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-21.1.8/openmp-21.1.8.src.tar.xz"
+  sha256 "856b023748b41ac7b2c83fd8e9f765ff48a4df2fe6777d2811ef7c7ed8f2f977"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    regex(/^llvmorg[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  # Ref: https://github.com/Homebrew/homebrew-core/issues/112107
+  keg_only "it can override GCC headers and result in broken builds"
+
+  depends_on "cmake" => :build
+  # depends_on "lit" => :build
+  uses_from_macos "llvm" => :build
+
+  resource "cmake" do
+    url "https://github.com/llvm/llvm-project/releases/download/llvmorg-21.1.8/cmake-21.1.8.src.tar.xz"
+    sha256 "85735f20fd8c81ecb0a09abb0c267018475420e93b65050cc5b7634eab744de9"
+
+    livecheck do
+      formula :parent
+    end
+  end
+
+  def install
+    if File.exist?("/tmp/dehancer-homebrew-build-for-macos13.txt")
+      ENV['MACOSX_DEPLOYMENT_TARGET']="13.0"
+      ohai "[dehancer] Building formula for macOS 13"
+    elsif File.exist?("/tmp/dehancer-homebrew-build-for-macos15.txt")
+      ENV['MACOSX_DEPLOYMENT_TARGET']="15.0"
+      ohai "[dehancer] Building formula for macOS 15"
+    else
+      odie "[dehancer] You must specify a macOS deployment target by creating a flag file in /tmp"
+    end
+
+    if ENV['HOMEBREW_OPTFLAGS']&.include?("westmere")
+      ENV['HOMEBREW_OPTFLAGS']='-march=x86-64 -arch x86_64'
+      ohai "[dehancer] HOMEBREW_OPTFLAGS value changed to: #{ENV["HOMEBREW_OPTFLAGS"]}"
+    end
+
+    odie "cmake resource needs to be updated" if version != resource("cmake").version
+
+    (buildpath/"src").install buildpath.children
+    (buildpath/"cmake").install resource("cmake")
+
+    # Disable LIBOMP_INSTALL_ALIASES, otherwise the library is installed as
+    # libgomp alias which can conflict with GCC's libgomp.
+    args = ["-DLIBOMP_INSTALL_ALIASES=OFF"]
+
+    system "cmake", "-S", "src", "-B", "build/shared", *std_cmake_args, *args
+    system "cmake", "--build", "build/shared"
+    system "cmake", "--install", "build/shared"
+
+    # system "cmake", "-S", "src", "-B", "build/static",
+    #                 "-DLIBOMP_ENABLE_SHARED=OFF",
+    #                 *std_cmake_args, *args
+    # system "cmake", "--build", "build/static"
+    # system "cmake", "--install", "build/static"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <omp.h>
+      #include <array>
+      int main (int argc, char** argv) {
+        std::array<size_t,2> arr = {0,0};
+        #pragma omp parallel num_threads(2)
+        {
+            size_t tid = omp_get_thread_num();
+            arr.at(tid) = tid + 1;
+        }
+        if(arr.at(0) == 1 && arr.at(1) == 2)
+            return 0;
+        else
+            return 1;
+      }
+    CPP
+    system ENV.cxx, "-Werror", "-Xpreprocessor", "-fopenmp", "test.cpp", "-std=c++11",
+                    "-I#{include}", "-L#{lib}", "-lomp", "-o", "test"
+    system "./test"
+  end
+end

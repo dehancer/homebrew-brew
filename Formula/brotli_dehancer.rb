74bc4130ec64d05a55051ab9828c60a34dbd58d8
Egor Egorov <me@egorfine.com>
2026-01-19 16:47:41 +0100

    brotli

---
+++ Formula/brotli_dehancer.rb
@@ -0,0 +1,41 @@
+class BrotliDehancer < Formula
+  desc "Generic-purpose lossless compression algorithm by Google"
+  homepage "https://github.com/google/brotli"
+  url "https://github.com/google/brotli/archive/refs/tags/v1.2.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/brotli-1.2.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/brotli-1.2.0.tar.gz"
+  sha256 "816c96e8e8f193b40151dad7e8ff37b1221d019dbcb9c35cd3fadbfe6477dfec"
+  license "MIT"
+  head "https://github.com/google/brotli.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    if File.exist?("/tmp/dehancer-homebrew-build-for-macos13.txt")
+      ENV['MACOSX_DEPLOYMENT_TARGET']="13.0"
+      ohai "[dehancer] Building formula for macOS 13"
+    elsif File.exist?("/tmp/dehancer-homebrew-build-for-macos15.txt")
+      ENV['MACOSX_DEPLOYMENT_TARGET']="15.0"
+      ohai "[dehancer] Building formula for macOS 15"
+    else
+      odie "[dehancer] You must specify a macOS deployment target by creating a flag file in /tmp"
+    end
+
+    if ENV['HOMEBREW_OPTFLAGS']&.include?("westmere")
+      ENV['HOMEBREW_OPTFLAGS']='-march=x86-64 -arch x86_64'
+      ohai "[dehancer] HOMEBREW_OPTFLAGS value changed to: #{ENV["HOMEBREW_OPTFLAGS"]}"
+    end
+
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_INSTALL_RPATH=#{rpath}", *std_cmake_args
+    system "cmake", "--build", "build", "--verbose"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+  end
+end
